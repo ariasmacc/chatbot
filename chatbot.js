a1d6fb796bdf24223d5f9c9f1ec889857ab6f539
ariasmacc <ariacaden10@gmail.com>
2026-03-16 13:27:30 +0800

    fix commonjs handler

--- chatbot.js
+++ chatbot.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Allow requests from your GitHub Pages site
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
